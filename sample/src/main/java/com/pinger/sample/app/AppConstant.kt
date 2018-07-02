3fedea4c23ccefe9140d1882b14080966ac2fc53
Pinger <wanitme@gmail.com>
2018-07-02 14:48:39 +0800

    更新了截屏功能

--- sample/src/main/java/com/pinger/sample/app/AppConstant.kt
+++ sample/src/main/java/com/pinger/sample/app/AppConstant.kt
@@ -10,4 +10,6 @@ object AppConstant {
     const val QQ_APP_ID = "1107010230"
     const val QQ_APP_KEY = "EsJ2Ki44eb9mgvo4"
 
+    const val SINA_APP_KEY = "2332189931"
+    const val SINA_APP_SECRET = "0390444ae665f63dddb131774e527849"
 }
